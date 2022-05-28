35b832152bcd6c4bb2453889ccb57063c9bfedb5
Patrick Shechet <patrick.shechet@gmail.com>
2022-05-28 12:42:31 -0600

    Minor formatting changes

--- tests/test_version_machine.py
+++ tests/test_version_machine.py
@@ -1,4 +1,3 @@
-from ensurepip import version
 from unittest import TestCase, main
 from unittest.mock import patch, mock_open
 from version_machine.core import version_travel
@@ -17,19 +16,27 @@ class TestVersionMachine(TestCase):
 
     def test_basic_major(self):
         version_travel(path="", increment_type="MAJOR")
-        self.mock_open.return_value.write.assert_called_once_with('__version__ = "1.0.0"')
+        self.mock_open.return_value.write.assert_called_once_with(
+            '__version__ = "1.0.0"'
+        )
 
     def test_basic_minor(self):
         version_travel(path="", increment_type="MINOR")
-        self.mock_open.return_value.write.assert_called_once_with('__version__ = "0.1.0"')
-        
+        self.mock_open.return_value.write.assert_called_once_with(
+            '__version__ = "0.1.0"'
+        )
+
     def test_basic_patch(self):
         version_travel(path="", increment_type="PATCH")
-        self.mock_open.return_value.write.assert_called_once_with('__version__ = "0.0.2"')
+        self.mock_open.return_value.write.assert_called_once_with(
+            '__version__ = "0.0.2"'
+        )
 
     def test_basic_override(self):
         version_travel(path="", increment_type="PATCH", override_version="9.9.9")
-        self.mock_open.return_value.write.assert_called_once_with('__version__ = "9.9.9"')
+        self.mock_open.return_value.write.assert_called_once_with(
+            '__version__ = "9.9.9"'
+        )
 
 
 if __name__ == "__main__":
